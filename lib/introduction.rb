e81855146fb6c277ec39481c222b46d41171842b
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-12 03:11:21 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction_with_language(name = "Josh", language)
+def introduction_with_language(name = "Dan", language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
